a6f5c866a8b7ad147ba8178090d74559e2c9fdd0
te26Oku <teturou@aw.wakwak.com>
2020-09-03 16:58:28 +0900

    ピンチでハンドメニューを表示するようにした

--- Assets/Scripts/HandTracking.cs
+++ Assets/Scripts/HandTracking.cs
@@ -1,21 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class HandTracking : MonoBehaviour
 {
+    [SerializeField] private Text text_isIndexPinching;
+    [SerializeField] private Text text_middlePinchStrength;
+    [SerializeField] private OVRHand rightHand;
+    [SerializeField] private OVRHand leftHand;
+    [SerializeField] private GameObject HandMenu;
+    [SerializeField] private Text text_HandMenuPosiotionX;
     
-    // Start is called before the first frame update
+    // state
+    private bool isLeftIndexPinching;
+    private bool enabledHandMenu;
+    private float HandMenuPositionX;
+    private bool releasedIndex;
+    private bool releasedMiddle;
+
+    // audio
+    private AudioSource audioSource;
+    [SerializeField] AudioClip showHandMenuAudio;
+    [SerializeField] AudioClip HideHandMenuAudio;
+
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();  
+
+        // ハンドメニューの初期化 
+        HandMenu.SetActive(false);
+        enabledHandMenu = false;
+    }
+    
+    
+    void Update()
+    {
+        if ((leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index) && leftHand.GetFingerIsPinching(OVRHand.HandFinger.Thumb))
+         || Input.GetKey(KeyCode.Space))
+        {
+                isLeftIndexPinching = true;
+        } else {
+                isLeftIndexPinching = false;
+        }
+
+        if (isLeftIndexPinching) {
+            if (!enabledHandMenu) {
+                audioSource.PlayOneShot(showHandMenuAudio);
+                HandMenu.SetActive(true);
+                enabledHandMenu = true;
+            }
+        } else {
+            if (enabledHandMenu) {
+                audioSource.PlayOneShot(HideHandMenuAudio);
+                HandMenu.SetActive(false);
+                enabledHandMenu = false;
+            }
+        }
+        
+        
+
+
+
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    
 
     
 }
