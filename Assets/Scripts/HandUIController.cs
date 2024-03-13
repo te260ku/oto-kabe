26ba2d3909a57704d13de6b1766363ac163ee17e
te260ku <teturou@aw.wakwak.com>
2024-03-13 18:21:53 +0900

    ハンドメニューの表示を切り替えるトグルを作成

--- Assets/Scripts/HandUIController.cs
+++ Assets/Scripts/HandUIController.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class HandUIController : MonoBehaviour
 {
     [SerializeField] GameObject uiObj;
+    [SerializeField] TextMeshProUGUI visibilitySwitcherButtonText;
+    [SerializeField] string visibilitySwitcherButtonTextValueWithON;
+    [SerializeField] string visibilitySwitcherButtonTextValueWithOFF;
     bool visibility;
+
     void Start()
     {
-        
+        visibility = true;
+        visibilitySwitcherButtonText.text = visibilitySwitcherButtonTextValueWithOFF;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A)) {
+            SwitchVisibility();
+        }
     }
 
     public void SwitchVisibility() {
         visibility = !visibility;
         uiObj.SetActive(visibility);
+        visibilitySwitcherButtonText.text = visibility ? visibilitySwitcherButtonTextValueWithOFF : visibilitySwitcherButtonTextValueWithON;
     }
 }
